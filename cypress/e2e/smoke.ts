94856169bac50ad2720aeb88b4893709ca496e71
Chris Arderne <chris@rdrn.me>
2022-04-13 16:32:22 +0100

    Add cypress tests

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -1,48 +1,29 @@
-import faker from "@faker-js/faker";
-
 describe("smoke tests", () => {
   afterEach(() => {
-    cy.cleanupUser();
+    //redis.flushall();
   });
 
-  it("should allow you to register and login", () => {
-    const loginForm = {
-      email: `${faker.internet.userName()}@example.com`,
-      password: faker.internet.password(),
-    };
-    cy.then(() => ({ email: loginForm.email })).as("user");
+  it("should allow you to create a room", () => {
+    cy.visit("/");
+    cy.findByRole("link", { name: /create/i }).click();
+    cy.wait(200);
+    cy.get("label input").type("This is a statement");
+    cy.get("[type='submit']").click();
+    cy.wait(50);
+    cy.get("[type='submit']").click();
+    cy.wait(50);
+    cy.get("button").click();
+    cy.findByText(`"Nobody matched!"`);
 
     cy.visit("/");
-    cy.findByRole("link", { name: /sign up/i }).click();
-
-    cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
-    cy.findByLabelText(/password/i).type(loginForm.password);
-    cy.findByRole("button", { name: /create account/i }).click();
-
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByRole("button", { name: /logout/i }).click();
-    cy.findByRole("link", { name: /log in/i });
+    cy.findByRole("textbox").type("fake-room");
+    cy.findByRole("button").click();
   });
 
-  it("should allow you to make a note", () => {
-    const testNote = {
-      title: faker.lorem.words(1),
-      body: faker.lorem.sentences(1),
-    };
-    cy.login();
+  it("should not allow you join a non-existing room", () => {
     cy.visit("/");
-
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByText("No notes yet");
-
-    cy.findByRole("link", { name: /\+ new note/i }).click();
-
-    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
-    cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
-    cy.findByRole("button", { name: /save/i }).click();
-
-    cy.findByRole("button", { name: /delete/i }).click();
-
-    cy.findByText("No notes yet");
+    cy.findByRole("textbox").type("fake-room");
+    cy.findByRole("button").click();
+    cy.get("div.visible").findByText("No such room!");
   });
 });
