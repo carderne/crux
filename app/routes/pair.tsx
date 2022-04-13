b593d02f5d70f6a50042e6201bb09bf26ee74a5d
Chris Arderne <chris@rdrn.me>
2022-04-13 12:25:57 +0100

    Update algo

--- app/routes/pair.tsx
+++ app/routes/pair.tsx
@@ -1,24 +1,47 @@
 import type { ActionFunction } from "@remix-run/node";
 import { redis, redisSet } from "~/utils/redis.server";
 
-const colors = ["red", "blue", "green", "purple", "yellow", "pink"];
+const colors = [
+  "#1f78b4",  // db
+  "#33a02c",  // dg
+  "#e31a1c",  // dr
+  "#ff7f00",  // do
+  "#6a3d9a",  // dp
+  "#b15928",  // dy
+  "#a6cee3",  // lb
+  "#b2df8a",  // lg
+  "#fb9a99",  // lr
+  "#fdbf6f",  // lo
+  "#cab2d6",  // lp
+  "#ffff99",  // ly
+];
 
-export const calculatePairs = (statements, ratings) => {
-  const doneIds = [];
+export const calculate = (statements, people) => {
   const pairings = {};
-  statements.forEach((s, i) => {
-    const useKeys = Object.keys(ratings).filter((id) => !doneIds.includes(id));
-    const minId = useKeys.reduce((a, b) =>
-      ratings[a][i] < ratings[b][i] ? a : b
-    );
-    const maxId = useKeys.reduce((a, b) =>
-      ratings[a][i] > ratings[b][i] ? a : b
-    );
-    if (true) {
-      doneIds.push(minId, maxId);
-      const color = colors[i];
-      pairings[minId] = { statement: s, color: color };
-      pairings[maxId] = { statement: s, color: color };
+  let colorIdx = 0;
+  Object.entries(people).forEach(([p, ratings]) => {
+    if (!(p in pairings)) {
+      let [partner, most, s] = ["", -1, null];
+      ratings.forEach((r, i) => {
+        Object.entries(people).forEach(([o, o_ratings]) => {
+          if (!(o in pairings || o === p)) {
+            const score = Math.abs(r - o_ratings[i]);
+            if (score > most) {
+              most = score;
+              s = i;
+              partner = o;
+            }
+          }
+        });
+      });
+      if (partner !== "") {
+        const color = colors[colorIdx];
+        colorIdx++;
+        pairings[p] = { statement: statements[s], color };
+        pairings[partner] = { statement: statements[s], color };
+      } else {
+        pairings[p] = { statement: "Nobody matched!", color: "white" };
+      }
     }
   });
   return pairings;
@@ -45,7 +68,7 @@ export const action: ActionFunction = async ({ request }) => {
 
   if (statements?.length === 0 || ratings?.length === 0) return null;
 
-  const pairings = JSON.stringify(calculatePairs(statements, ratings));
+  const pairings = JSON.stringify(calculate(statements, ratings));
   await redisSet(`${room}:pairings`, pairings);
 
   return null;
